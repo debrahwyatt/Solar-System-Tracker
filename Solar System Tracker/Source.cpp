fd53038828c9486f449f9b7c183f58268af5735a
debrahrwyatt@gmail.com <debrahrwyatt@gmail.com>
2020-02-02 17:04:04 -0800

    Adding a long overdue C++ project

--- Solar System Tracker/Source.cpp
+++ Solar System Tracker/Source.cpp
@@ -4,36 +4,34 @@
 
 using std::cout;
 using std::endl;
-using std::setprecision; 
+using std::setprecision;
 using std::fixed;
 using std::setw;
 
 double Pi = 3.14159265359;
-bool B1;
 
 //Earth's Information
 double AstronomicalUnit = 149597870700; //In meters 
 double EarthX, EarthY; //Earths coordinates from Sun
 
-double EarthSecond = ((2*Pi) / (365.24*24*60*60)); // angle change per second
+double EarthSecond = ((2 * Pi) / (365.24 * 24 * 60 * 60)); // angle change per second
 double EarthVelocity = EarthSecond*AstronomicalUnit; // Distance traveled per second
 
-double EarthCurrentTime; //In seconds
+double EarthCurrentTime; //Angle Change Per Second
 double EarthCurrentDistance; //In meters
 
-double EarthCircumfrance = 4007500; //in meters
-double EarthSpin = EarthCircumfrance*EarthSecond; //in m/s
+double EarthCircumfrance = 40075000; //in meters
+double EarthSpin = EarthCircumfrance/(60*60*24); //in m/s
 double EarthCurrentSpin; // in meters
-
 
 
 int main()
 {
 	//Earth's Orbit
-	
+
 	cout << "Earth:\n";
 
-	while (EarthCurrentTime >= 0)
+	while (EarthCurrentTime >= 0 && EarthCurrentTime <= (EarthSecond *60 *60 *24))
 	{
 		EarthCurrentTime = EarthCurrentTime + EarthSecond;
 		EarthCurrentDistance = EarthCurrentDistance + EarthVelocity;
@@ -43,17 +41,11 @@ int main()
 		EarthY = sin(EarthCurrentTime)*AstronomicalUnit;
 
 
-		cout << setprecision(3) << fixed << "X: " << EarthX << "m, Y: " << EarthY
-			<< "km, Distance Traveled: " << EarthCurrentDistance << "m, Travel Velocity: " << EarthVelocity << "m/s\r";
-		if (B1 == false)
-		{
-			cout << endl;
-			B1 = true;
-		}
-		cout << "Angular Velocity: " << EarthSpin << "m/s, Distance spun: " <<EarthCurrentSpin << "m\r";
+		cout << setprecision(3) << fixed /*<< "X: " << EarthX << "m, Y: " << EarthY*/
+			<< "km, Distance Traveled: " << EarthCurrentDistance << "m, Travel Velocity: " << EarthVelocity << "m/s"
+			<< " Angular Velocity: " << EarthSpin << "m/s, Distance spun: " << EarthCurrentSpin << "m\r";
 
 
 		Sleep(1000);
 	}
-	
 }
